98a64eec178c81e5aac52baeefde1ab4f1a9960a
Sanju tl <sanjutl19@gmail.com>
2026-07-14 12:49:25 +0530

    first commit

---
+++ backend/multer/multer.js
@@ -0,0 +1,27 @@
+import multer from "multer";
+import path from "path"
+
+
+const productStorage=multer.diskStorage({
+    destination:"./uploads/products",
+    filename:(req,file,cb)=>{
+        const ext=path.extname(file.originalname);
+        cb(null,`product-${Date.now()}${ext}`)
+    }
+})
+
+const productFilter=(req,file,cb)=>{
+    if(file.mimetype.startsWith("image/")){
+        cb(null,true)
+    }else{
+        cb(new Error("Only image files are allowed"),false)
+    }
+}
+
+export const productMulter=multer({
+    storage:productStorage,
+    fileFilter:productFilter,
+    limits:{
+        fileSize:5*1024*1024,
+    },
+})
